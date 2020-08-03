4cae0ebd6323cedefca5682cd93c47734f038360
dzinoviev <danzino21@gmail.com>
2020-08-03 09:35:48 +0300

    Update: show image on error

--- src/components/error-indicator/error-indicator.jsx
+++ src/components/error-indicator/error-indicator.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import icon from './death-star.png';
 
 const ErrorIndicator = () => {
   return (
     <div className="error-indicator">
+      <img src={icon} alt="Error"/>
       <h4 className="text-danger">An unexpted error has been occured.</h4>
       <p className="text-danger">Don't worry, we already sent some droids to fix a problem.</p>
     </div>
