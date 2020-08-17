0df2502ccdce0cbbdee0d7c6773eb8955b93227c
dzinoviev <danzino21@gmail.com>
2020-08-17 09:26:20 +0300

    refactor: itemlist

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -5,25 +5,27 @@ import Header from "../header/header";
 import RandomPlanet from "../random-planet/random-planet";
 import ItemList from "../item-list/item-list";
 import PersonDetails from "../person-details/person-details";
+import SwapiService from "../../services/SwapiService";
 
 class App extends Component {
+  swapiService = new SwapiService();
 
   state = {
     showRandomPlanet: true,
     selectedPerson: null,
-  }
+  };
 
   toggleRandomPlanet() {
     this.setState((state) => ({
-      showRandomPlanet: !state.showRandomPlanet
+      showRandomPlanet: !state.showRandomPlanet,
     }));
   }
 
   onChangeSelected(id) {
     console.log(`App: onChangeSelected with id ${id}`);
     this.setState({
-      selectedPerson: id
-    })
+      selectedPerson: id,
+    });
   }
 
   render() {
@@ -31,21 +33,56 @@ class App extends Component {
       <div className="app container">
         <Header />
         {this.state.showRandomPlanet ? <RandomPlanet /> : null}
-        
-        <button className="btn btn-warning mb-3" onClick={() => this.toggleRandomPlanet()}>
+
+        <button
+          className="btn btn-warning mb-3"
+          onClick={() => this.toggleRandomPlanet()}
+        >
           Toggle random planet
         </button>
-        <div className="row">
+
+        <div className="row mb-2">
           <div className="col-md-4">
-            <ItemList onChangeSelected={(id) => this.onChangeSelected(id)} />
+            <ItemList
+              onChangeSelected={(id) => this.onChangeSelected(id)}
+              getData={() => this.swapiService.getAllPeoples()}
+              renderItem={(item) => (
+                <React.Fragment>
+                  <span>{item.name}</span>{" "}
+                  <em>
+                    ({item.birthYear}, {item.gender})
+                  </em>
+                </React.Fragment>
+              )}
+            />
           </div>
           <div className="col-md-8">
-            <PersonDetails selectedPerson={this.state.selectedPerson}/>
+            <PersonDetails selectedPerson={this.state.selectedPerson} />
+          </div>
+        </div>
+
+        <div className="row mb-2">
+          <div className="col-md-4">
+            <ItemList
+              onChangeSelected={(id) => this.onChangeSelected(id)}
+              getData={() => this.swapiService.getAllSpaceships()}
+              renderItem={(item) => (
+                <React.Fragment>
+                  <span>{item.name}</span>{" "}
+                  <em>
+                    ({item.manufacturer})
+                  </em>
+                </React.Fragment>
+              )}
+            />
+          </div>
+          <div className="col-md-8">
+            <PersonDetails selectedPerson={this.state.selectedPerson} />
           </div>
         </div>
       </div>
     );
   }
-};
+}
 
 export default App;
--- src/components/item-list/item-list.jsx
+++ src/components/item-list/item-list.jsx
@@ -5,38 +5,36 @@ import SwapiService from "../../services/SwapiService";
 import Spinner from "../spinner/spinner";
 
 class ItemList extends Component {
-  swapiService = new SwapiService();
-
   state = {
-    peopleList: null,
+    itemList: null,
   };
 
   componentDidMount() {
-    this.swapiService.getAllPeoples().then((peoples) => {
+    this.props.getData().then((items) => {
       this.setState({
-        peopleList: peoples.slice(0, 6), // Trim to 6 peoples
+        itemList: items.slice(0, 6), // Trim to 6 peoples
       });
     });
   }
 
   render() {
-    const { peopleList } = this.state;
-    const { onChangeSelected } = this.props;
+    const { itemList } = this.state;
+    const { onChangeSelected, renderItem } = this.props;
 
-    if (!peopleList) {
+    if (!itemList) {
       return <Spinner />;
     }
 
     return (
       <div className="item-list">
         <ul className="list-group">
-          {peopleList.map(({ id, name }) => (
+          {itemList.map((item) => (
             <li
               className="list-group-item"
-              key={id}
-              onClick={() => onChangeSelected(id)}
+              key={item.id}
+              onClick={() => onChangeSelected(item.id)}
             >
-              {name}
+              {renderItem(item)}
             </li>
           ))}
         </ul>
